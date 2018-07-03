8cdfe932a08264bb6cc959ef262cf78aa09e6125
hayz999 <hlzulkoski@gmail.com>
2018-07-03 15:57:13 -0600

    updated cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const cors = require('cors')
 app.use(bodyParser.json())
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://wine-about-it.firebaseapp.com/',
   optionsSuccessStatus: 200
 }))
 
